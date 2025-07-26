1ccdf7d412e46af306acf43feecd6bf80a456c83
Daniel Kandie <danielkandie14@gmail.com>
2025-07-26 17:32:17 +0300

    added getStaticProps

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,31 +1,15 @@
 // pages/posts.tsx
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 import { type PostProps } from "@/interfaces";
 
-const PostsPage = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+interface PostsPageProps {
+  posts: PostProps[];
+}
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const data = await res.json();
-
-      const formattedPosts: PostProps[] = data.slice(0, 10).map((post: any) => ({
-        id: post.id,
-        title: post.title,
-        content: post.body, // JSONPlaceholder uses `body`
-        userId: post.userId,
-      }));
-
-      setPosts(formattedPosts);
-    };
-
-    fetchPosts();
-  }, []);
-
+const PostsPage = ({ posts }: PostsPageProps) => {
   return (
     <div>
       <Header />
@@ -38,13 +22,31 @@ const PostsPage = () => {
               title={post.title}
               content={post.content}
               userId={post.userId}
-              id={post.id} // include this for consistency
+              id={post.id}
             />
           ))}
         </div>
       </main>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  const formattedPosts: PostProps[] = data.slice(0, 10).map((post: any) => ({
+    id: post.id,
+    title: post.title,
+    content: post.body, // Map 'body' to 'content'
+    userId: post.userId,
+  }));
+
+  return {
+    props: {
+      posts: formattedPosts,
+    },
+  };
 };
 
 export default PostsPage;
